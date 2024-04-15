f668d78a395cb5d15799eb61f0c3e6d50ff07195
dmincberger <mincbergerdominik@gmail.com>
2024-04-15 09:02:03 +0200

    pierwsza czesc aut

--- app01/src/applekcja01/Appz05.vue
+++ app01/src/applekcja01/Appz05.vue
@@ -5,6 +5,9 @@
             <h3 :style="{background:auto.hex_color}">kolor auta</h3>
             <h3 v-if="auto.damaged" style="background-color: red;">USZKODZONY</h3>
             <h3 v-else style="background-color: green;">SPRAWNY</h3>
+            <img :src="auto.image">
+            <h2>id: {{auto.id }}</h2>
+            <h2>name: {{ auto.car_name }}</h2>
         </div>
     </template>
 </div>
@@ -22,4 +25,18 @@ export default {
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+    padding: 0px;
+}
+#main {
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+}
+
+#main>* {
+    padding: 20px;
+}
+
 </style>
